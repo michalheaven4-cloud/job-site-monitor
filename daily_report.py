3a40c81558da46548c974491c9ee330b4bf688bb
MOONDONGHWAN\문동환 <nrg5446@gmail.com>
2025-09-15 15:39:30 +0900

    수정

--- daily_report.py
+++ daily_report.py
@@ -35,11 +35,7 @@ class AlbamonAnalyzerCLI:
         self.base_url = 'https://bff-general.albamon.com'
         self.headers = {
             'Accept': '*/*',
-            "User-Agent": (
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                "AppleWebKit/537.36 (KHTML, like Gecko) "
-                "Chrome/114.0.0.0 Safari/537.36"
-            )
+            'User-Agent': 'job-site-monitor/1.0.0',
             'origin': 'https://www.albamon.com',
             'Content-Type': 'application/json',
             'cookie': (
@@ -547,11 +543,7 @@ def send_report_to_api(all_result, today_result):
         headers = {
             'Content-Type': 'application/json',
             'Authorization': f'Bearer {api_password}',
-            "User-Agent": (
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-                "AppleWebKit/537.36 (KHTML, like Gecko) "
-                "Chrome/114.0.0.0 Safari/537.36"
-            )
+            'User-Agent': 'job-site-monitor/1.0.0'
         }
 
         # API 전송 전에 JSON 출력
